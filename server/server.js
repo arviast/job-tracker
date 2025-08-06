e8fa5e9f8316dd6806646654478b7b986191da31
Edwin Jamsranjav <edwinjamsranjav@gmail.com>
2025-08-06 16:15:08 +0800

    User register, mongodb setup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const jobRoutes = require("./routes/jobRoutes");
+const authMiddleware = require("./middleware/authMiddleware");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/jobs", authMiddleware, jobRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
